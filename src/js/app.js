f88b8186235c396cdb0562ab87c09156044b4052
Elena Slyusareva <sportgirl55@bk.ru>
2021-03-10 21:31:01 +0500

    New version

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,11 @@
-console.log('it works!');
+// TODO: write code here
+
+// comment this to pass build
+// const unusedVariable = 'variable';
+
+// for demonstration purpose only
+export default function demo(value) {
+  return value;
+}
+
+console.log('app.js included');
