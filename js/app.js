3a6d49fb039f0c408f26cc195ffc8351ddb46f22
AppVeyor <ci@appveyor.com>
2021-03-10 16:32:00 +0000

    f88b818

---
+++ js/app.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = demo;
+
+// TODO: write code here
+// comment this to pass build
+// const unusedVariable = 'variable';
+// for demonstration purpose only
+function demo(value) {
+  return value;
+}
+
+console.log('app.js included');
